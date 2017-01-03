bfe0e03c49826ddff24cadc87e0a5d5b2de37e47
shadowcho <niki.sotirov5@gmail.com>
2017-01-04 00:25:11 +0200

    Add header file and implement a prototype of linked list

---
+++ CppApplication_1/Hangman.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Hangman.h
+ * Author: shadowcho
+ *
+ * Created on January 4, 2017, 12:04 AM
+ */
+
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+typedef unsigned char BYTE;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HANGMAN_H */
+
