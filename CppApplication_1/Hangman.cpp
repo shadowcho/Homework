7ed07f69721e3c533e5d090caf83104fd4189ace
shadowcho <niki.sotirov5@gmail.com>
2017-01-03 23:22:13 +0200

    second commit to add source code

---
+++ CppApplication_1/Hangman.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Hangman.cpp
+ * Author: shadowcho
+ *
+ * Created on January 3, 2017, 8:21 PM
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    printf("Niki");
+    
+    return 0;
+}
+
---
+++ CppApplication_1/build/Debug/GNU-Linux/Hangman.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/Hangman.o: Hangman.cpp
